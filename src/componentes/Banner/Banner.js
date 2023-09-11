e126975b6d3ca899286016d0880db32b1d7b74ce
Cristiane <clsilva_@hotmail.com>
2023-09-11 18:15:44 -0300

    Primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX - Pesquisar sobre oque é
+    return (
+        <header className='banner'>
+            <img src="\imagens\banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
